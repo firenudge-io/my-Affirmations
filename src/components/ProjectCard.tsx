6a6fe66187290ac0b46f2445ed28e7dafaf2c7a3
Personalised Account <109010929+dependabot-pr@users.noreply.github.com>
2023-02-13 16:58:57 +0000

    `visual changes`

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -23,30 +23,29 @@ export const ProjectCard = () => {
     fetchData();
 
     return (
-        <div className="h-56 w-3/4 lg:w-10/12 bg-gray-50 dark:bg-gray-800 text-center p-10 shadow-md shadow-gray-200 dark:shadow-gray-600 rounded-xl mx-auto">
-
+        <div className="w-3/4 lg:w-10/12 bg-gray-50 dark:bg-gray-800 text-center p-10 shadow-md shadow-gray-200 dark:shadow-gray-600 rounded-xl mx-auto">
 
             {
                 head.length === 0 ?
                     <Spinner />
                     :
                     <div>
-                        <h1 className="text-3xl">
+                        <h1 className="text-3xl lg:text-6xl font-mono font-bold">
                             {head[num]}
                         </h1>
 
-                        <p className="text-xl mt-4">
+                        <p className="text-xl lg:text-3xl font-serif font-normal mt-6">
                             {body[num]}
                         </p>
 
                         <div className="flex flex-row justify-around mt-10">
                             {
                                 num === 0 ?
-                                    <button type="button" className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-300 focus:outline-none bg-gray-100 rounded-lg border border-gray-200  focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-700 dark:text-gray-400 dark:border-gray-600 cursor-not-allowed" disabled>
+                                    <button type="button" className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-300 bg-gray-100 rounded-lg border border-gray-200  dark:bg-gray-700 dark:text-gray-400 dark:border-gray-600 cursor-not-allowed invisible" disabled>
                                         Prev
                                     </button>
                                     :
-                                    <button type="button" className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                                    <button type="button" className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
                                         onClick={() => {
                                             if (num > 0) {
                                                 setNum(num - 1);
@@ -59,12 +58,12 @@ export const ProjectCard = () => {
 
                             {
                                 num === max ?
-                                    <button type="button" className="text-gray-400 bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 focus:outline-none dark:focus:ring-blue-800 cursor-not-allowed" disabled>
+                                    <button type="button" className="text-gray-400 bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 cursor-not-allowed invisible" disabled>
                                         Next
                                     </button>
                                     :
                                     <button type="button"
-                                        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                                        className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700"
                                         onClick={() => {
                                             if (num < max) {
                                                 setNum(num + 1);
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,7 +2,7 @@ import { ProjectCard } from "../components/ProjectCard"
 
 export const Dashboard = () => {
     return (
-        <div className="h-screen w-10/12 lg:w-1/2 mx-auto flex flex-row mt-32">
+        <div className="w-full lg:w-1/2 mx-auto flex flex-row mt-32 mb-96">
             <ProjectCard />
         </div>
     )
